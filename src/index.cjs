a15e78ab0216e2ee6b962b0ff463005658f87723
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2023-04-06 18:31:37 +0900

    prepaere for type support

--- src/index.cjs
+++ src/index.cjs
@@ -1,6 +1,6 @@
-const Controller = require('./controller')
-const Worker = require('./worker');
-const SharedData = require('./sharedData/data');
-SharedData.funcs = require('./sharedData/funcs/overwrite')
+module.exports.Controller = require('./controller')
+module.exports.Worker = require('./worker');
+module.exports.SharedData = require('./sharedData/data');
+module.exports.SharedData.funcs = require('./sharedData/funcs/overwrite')
 
-module.exports = {Controller, Worker, SharedData}
+
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,10 @@
 import module from "module";
 const require = module.createRequire(import.meta.url)
 const namespace = require('./index.cjs');
-export const { Controller, Worker, SharedData } = namespace
+
+export const Controller = require('./controller')
+export const Worker = require('./worker');
+export const SharedData = require('./sharedData/data');
+SharedData.funcs = require('./sharedData/funcs/overwrite')
+
 export default namespace 
